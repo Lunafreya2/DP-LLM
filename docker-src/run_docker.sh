f608bcb1467c7501f8770e24b4636c77dfaed387
Hamis Haider <hamis.haider@usask.ca>
2024-01-02 16:27:45 -0600

    add run docker script

---
+++ docker-src/run_docker.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+docker run  --rm --gpus all \
+-v "/media/uofsko-lab/c61d4ea0-b8fe-4205-9815-f7d3d054043c/hamis/":"/workspace" \
+-v /tmp/.X11-unix:/tmp/.X11-unix \
+-e DISPLAY=$DISPLAY --net=host -it hamis_dpllm
+# --user uofsko-lab:1000 \
