199112251c259d4f147ff0d0e2587623b1c26bb3
hamis.haider <hamis.haider@usask.ca>
2024-01-02 20:40:20 -0600

    add gen gpt2 pysrc file

---
+++ py-src/gen_gpt2.py
@@ -0,0 +1,32 @@
+# import datasets
+import sys
+
+import torch
+from transformers import GPT2LMHeadModel, GPT2Tokenizer
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+emotion = sys.argv[1]
+# dataset_e = datasets.load_dataset("dair-ai/emotion", name="split", split="test")
+
+model = GPT2LMHeadModel.from_pretrained("./finetunedmodel")
+tokenizer = GPT2Tokenizer.from_pretrained("./finetunedtokenizer")
+
+model.eval()
+
+input_ids = (
+    torch.tensor(tokenizer.encode(f"[BOS]{emotion}[SEP]")).unsqueeze(0).to(device)
+)  # Empty string as a prompt
+
+output = model.generate(
+    input_ids,
+    max_length=50,
+    num_return_sequences=1,  # Number of sequences to return
+    num_beams=10,  # Number of beams for beam search
+    temperature=0.2,
+    top_k=25,
+    no_repeat_ngram_size=3,
+    early_stopping=True,
+)
+
+tokenizer.decode(output[0], skip_special_tokens=False)
